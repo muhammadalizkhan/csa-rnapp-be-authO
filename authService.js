e1ee3e0ecbd8fda3fcbae463095cee48d128b1c8
Muhamad Ali Khan <your_username@users.noreply.github.com>
2024-12-04 14:27:25 +0500

    deploy: auth0 vercel deploy

--- authService.js
+++ authService.js
@@ -1,25 +1,23 @@
 const axios = require('axios');
 
-const AUTH0_DOMAIN = process.env.AUTH0_DOMAIN;
-const AUTH0_CLIENT_ID = process.env.AUTH0_CLIENT_ID;
-const AUTH0_CLIENT_SECRET = process.env.AUTH0_CLIENT_SECRET;
-
 const getAccessToken = async () => {
-    try {
-        const response = await axios.post(`https://${AUTH0_DOMAIN}/oauth/token`, {
-            client_id: AUTH0_CLIENT_ID,
-            client_secret: AUTH0_CLIENT_SECRET,
-            audience: `https://${AUTH0_DOMAIN}/api/v2/`,
-            grant_type: 'client_credentials',
-        });
+  try {
+    console.log("Sending request to Auth0...");
+    const response = await axios.post(`https://${process.env.AUTH0_DOMAIN}/oauth/token`, {
+      client_id: process.env.AUTH0_CLIENT_ID,
+      client_secret: process.env.AUTH0_CLIENT_SECRET,
+      audience: `https://${process.env.AUTH0_DOMAIN}/api/v2/`,
+      grant_type: 'client_credentials',
+    });
 
-        const accessToken = response.data.access_token;
-        console.log('Access Token:', accessToken);
-        return accessToken;
-    } catch (error) {
-        console.error('Error obtaining access token:', error);
-        throw new Error('Unable to get access token');
-    }
+    console.log("Response from Auth0:", response.data);
+    const accessToken = response.data.access_token;
+    console.log('Access Token:', accessToken);
+    return accessToken;
+  } catch (error) {
+    console.error('Error obtaining access token:', error);
+    throw new Error('Unable to get access token');
+  }
 };
 
 module.exports = { getAccessToken };
